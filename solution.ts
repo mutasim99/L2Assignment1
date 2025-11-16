6e019fda954726a64b2fe9ea5af3386f87debe43
mutasim99 <virginmr882@gmail.com>
2025-11-17 02:36:49 +0600

    solved problem3

--- solution.ts
+++ solution.ts
@@ -8,6 +8,31 @@ function formatValue(value: string | number | boolean): string | number | boolea
     };
 };
 
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(true));
+
+function getLength(value: string | any[]) {
+    if (typeof value === 'string') {
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
+    };
+
+    return 0;
+};
+
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age
+    };
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    };
+};
+
+
+

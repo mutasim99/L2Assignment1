e29de0f81d08046717bea93cfc156bac01bf3c10
mutasim99 <virginmr882@gmail.com>
2025-11-17 03:54:48 +0600

    solve problem six

--- solution.ts
+++ solution.ts
@@ -64,7 +64,14 @@ interface Book {
     author: string;
     publishedYear: number;
     isAvailable: boolean
-}
+};
+
+function printBookDetails(book: Book): void {
+    const availability = book.isAvailable ? 'Yes' : 'No'
+    console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`);
+};
+
+
 
 
 
